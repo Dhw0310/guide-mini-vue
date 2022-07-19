9c3c429b7b0899d796451bd3a2a12f830875d5e2
乒乒乓乓 <dhw0310life@163.com>
2022-07-19 23:42:57 +0800

    实现readonly

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,20 +1,12 @@
-import { track, trigger } from "./effect"
+import { mutableHandlers, readonlyHandlers } from "./baseHandler"
 
 export function reactive(raw) {
-  return new Proxy(raw, {
-    get(target, key) {
-      const res = Reflect.get(target, key)
+  return createActionObject(raw, mutableHandlers)
+}
+export function readonly(raw) {
+  return createActionObject(raw, readonlyHandlers)
+}
 
-      // TODO 收集依赖
-      track(target, key)
-      return res
-    },
-    set(target, key, value) {
-      const res = Reflect.set(target, key, value)
-
-      // TODO 触发依赖
-      trigger(target, key)
-      return res
-    }
-  })
+function createActionObject(raw: any, baseHandler) {
+  return new Proxy(raw, baseHandler)
 }
