ec7f2a368c5b8b366e366d32fd09d6ab4e0a5899
乒乒乓乓 <dhw0310life@163.com>
2022-08-03 17:27:37 +0800

    proxyRefs

--- src/reactivity/tests/ref.spec.ts
+++ src/reactivity/tests/ref.spec.ts
@@ -1,6 +1,6 @@
 import { effect } from "../effect"
 import { reactive } from "../reactive";
-import { isRef, ref, unRef } from "../ref"
+import { isRef, proxyRefs, ref, unRef } from "../ref"
 describe("ref", () => {
   it("happy path", () => {
     const a = ref(1)
@@ -53,5 +53,25 @@ describe("ref", () => {
     const a = ref(1)
     expect(unRef(a)).toBe(1)
     expect(unRef(1)).toBe(1)
+  })
+  it('proxyRefs', () => {
+    const user = {
+      age: ref(18),
+      name: 'abcd'
+    }
+    // get -> age (ref) 那么就返回 .value
+    // not ref 返回  -> value
+    const proxyUser = proxyRefs(user)
+    expect(user.age.value).toBe(18)
+    expect(proxyUser.age).toBe(18)
+    expect(proxyUser.name).toBe('abcd')
+
+    proxyUser.age = 20
+    expect(user.age.value).toBe(20)
+    expect(proxyUser.age).toBe(20)
+
+    proxyUser.name = ref(20)
+    expect(proxyUser.age).toBe(20)
+    expect(user.age.value).toBe(20)
   })
 })
